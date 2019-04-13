1176ef74d9eb2a6df17f9bf4d8ecb6d1cd57dfef
Michael West <quagly@gmail.com>
2019-04-13 05:34:36 -0700

    complete 10.14

--- chap10/Element.scala
+++ chap10/Element.scala
@@ -16,16 +16,40 @@ abstract class Element {
   def width: Int =
     if (height == 0) 0 else contents(0).length
 
-  def above(that: Element): Element =
-    elem(this.contents ++ that.contents)
+  def above(that: Element): Element = {
+    val this1 = this widen that.width
+    val that1 = that widen this.height
+    elem(this1.contents ++ that1.contents)
+  }
 
   def beside(that: Element): Element = {
+    val this1 = this heighten that.height
+    val that1 = that heighten this.height
     elem(
       for (
-           (line1, line2) <- this.contents zip that.contents
+           (line1, line2) <- this1.contents zip that1.contents
          ) yield line1 + line2
     )
   }
+
+  // match widths of Elements
+  def widen(w: Int): Element =
+    if (w <= width) this
+    else {
+      // left and right padding to widen
+      val left = elem(' ', (w - width) / 2, height)
+      val right = elem(' ', w - width - left.width , height)
+      left beside this beside right
+    }
+
+  // match heights of Elements
+  def heighten(h: Int): Element =
+    if (h <= height) this
+    else {
+      val top = elem(' ', width, (h - height) / 2)
+      val bot = elem(' ', width, h - height - top.height)
+      top above this above bot
+    }
 
   override def toString = contents mkString "\n"
 }
