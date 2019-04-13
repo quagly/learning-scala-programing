9141ff1afc3f1a2cceaae6a5ba5471f9c33adfd2
Michael West <quagly@gmail.com>
2019-04-13 04:43:47 -0700

    working 10.13, completed listing 10.11

--- chap10/Element.scala
+++ chap10/Element.scala
@@ -1,3 +1,5 @@
+import Element.elem
+
 abstract class Element {
   def contents: Array[String]
   /* parameterless methods are used when there are no parameters and
@@ -11,7 +13,33 @@ abstract class Element {
   def height: Int = contents.length
   // equivilent height field
   // val height = contents.length
-  def width: Int = if (height == 0) 0 else contents(0).length
+  def width: Int =
+    if (height == 0) 0 else contents(0).length
 
+  def above(that: Element): Element =
+    elem(this.contents ++ that.contents)
 
+  def beside(that: Element): Element = {
+    elem(
+      for (
+           (line1, line2) <- this.contents zip that.contents
+         ) yield line1 + line2
+    )
+  }
+
+  override def toString = contents mkString "\n"
+}
+
+// compantion singleton for factory methods
+// a nice interface for client so they don't need to
+// know about the object hierarcy and different object types
+object Element {
+  def elem(contents: Array[String]): Element =
+    new ArrayElement(contents)
+
+  def elem(chr: Char, width: Int, height: Int): Element =
+    new UniformElement(chr, width, height)
+
+  def elem(line: String): Element =
+    new LineElement(line)
 }
