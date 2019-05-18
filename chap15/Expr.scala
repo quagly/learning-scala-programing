9d17f96466c611f6f960bb56cc3c267b86c2c9a1
Michael West <quagly@gmail.com>
2019-05-18 06:38:46 -0700

    started chap15

---
+++ chap15/Expr.scala
@@ -0,0 +1,8 @@
+// this could also be a trait
+abstract class Expr
+
+case class Var(name: String) extends Expr
+case class Number(num: Double) extends Expr
+case class UnOp(operator: String, arg: Expr) extends Expr
+case class BinOp(operator: String,
+  left: Expr, right: Expr) extends Expr
