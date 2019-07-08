958ef7e8337613f645856c36d65fda8afb8a0d70
Michael West <quagly@gmail.com>
2019-07-08 06:58:23 -0700

    completed 15.5

--- chap15/Expr.scala
+++ chap15/Expr.scala
@@ -1,5 +1,12 @@
-// this could also be a trait
-abstract class Expr
+/*
+ * see 15.6
+ * sealed super class prevents
+ * subclasses from being defined outside this file
+ *
+ * this allows the compiler to know whether every case is being pattern matched
+ */
+
+sealed abstract class Expr
 
 case class Var(name: String) extends Expr
 case class Number(num: Double) extends Expr
